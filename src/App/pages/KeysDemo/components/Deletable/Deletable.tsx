b810d5797ac974d8bb5e75acb1a2041f5c5a51a3
garipov adel <a.garipov@okenit.ru>
2023-10-19 14:36:03 +0300

    React keys

---
+++ src/App/pages/KeysDemo/components/Deletable/Deletable.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Button from "@/App/components/Button";
+
+type DeletableProps = React.PropsWithChildren<{
+    id: number;
+    onDelete: (id: number) => void;
+}>;
+
+const Deletable: React.FC<DeletableProps> = ({
+    id,
+    onDelete,
+    children,
+}) => {
+
+    const handleDelete = () => {
+        onDelete(id);
+    }
+
+    return (
+        <>
+            {children}
+            <Button onClick={handleDelete}>Добавить в начало</Button>
+        </>
+    )
+};
+
+export default Deletable;
