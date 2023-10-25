90e1d4adc7ccbda985b2ca1d668a5d9db2ddfeab
garipov adel <a.garipov@okenit.ru>
2023-10-25 15:04:24 +0300

    Router program navigation

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,20 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import React from 'react';
+
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 import Header from './pages/RouterPages/Header';
 import User from './pages/RouterPages/User';
 import Users from './pages/RouterPages/Users';
 
 const App = () => {
+
+    // const location = useLocation();
+
+    // React.useEffect(() => {
+    //     log('Страница поменялась!');
+    // }, [location]);
+
     return (
         <div className='app'>
             <BrowserRouter>
@@ -13,7 +22,10 @@ const App = () => {
             <div className="container">
                 <Routes>
                     <Route path="/" element={<Users/>}/>
-                    <Route path="/user" element={<User/>}></Route>
+                    <Route path="/user">
+                        <Route path=":id" element={<User/>}></Route>
+                    </Route>
+                    <Route path="*" element={<Navigate to="/" replace/>}/>
                 </Routes>
                 </div>
             </BrowserRouter>
--- src/App/pages/RouterPages/User/User.tsx
+++ src/App/pages/RouterPages/User/User.tsx
@@ -1,6 +1,15 @@
+import { useParams } from "react-router-dom";
+
+import USERS from "@/config/users";
+
 const User = () => {
+
+    const {id} = useParams();
+
+    const user = USERS.find(user => user.id === id)
+
     return (
-        <div>User PAGE</div>
+        <div>Страница пользователя {user?.name}</div>
     )
 }
 
