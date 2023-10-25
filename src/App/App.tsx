5172353fbc9c06636ee2ea60e6852dd6013826c0
garipov adel <a.garipov@okenit.ru>
2023-10-25 12:49:37 +0300

    Create Rounter Pages

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,24 @@
 import './App.css';
-import KeysDemo from "@/App/pages/KeysDemo";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Header from './pages/RouterPages/Header';
+import User from './pages/RouterPages/User';
+import Users from './pages/RouterPages/Users';
 
 const App = () => {
-  return (
-    <div className='app'>
-      <div className="container">
-      <KeysDemo />
-      </div>
-    </div>
-  );
+    return (
+        <div className='app'>
+            <BrowserRouter>
+            <Header />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Users/>}/>
+                    <Route path="/user" element={<User/>}></Route>
+                </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/App/pages/RouterPages/User/User.tsx
@@ -0,0 +1,7 @@
+const User = () => {
+    return (
+        <div>User PAGE</div>
+    )
+}
+
+export default User;
